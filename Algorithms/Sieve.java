25bf290f551d72865ad69327200de98c7fe54e27
Raghavendrapara <raghavendra0360@gmail.com>
2020-09-24 23:27:59 +0530

    Updates File Structure

--- Algorithms/Sieve.java
+++ Algorithms/Sieve.java
@@ -1,36 +1,36 @@
-package Algorithms;
-
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-
-public class Sieve {
-
-	public static void main(String args[])
-	{
-		 Scanner sc=new Scanner(System.in);
-		    int n=sc.nextInt();
-		    int k=sc.nextInt();
-		    int m=sc.nextInt();
-		    int arr[]=new int[n];
-		    
-		    for(int i=0;i<n;i++)
-		    arr[i]=sc.nextInt();
-
-		    int sum=arr[m];
-
-		     for(int j=0;j<=k;j++)
-		    sum=sum^arr[j+m];
-
-		    System.out.println(sum);
-}
-}
-class AB
+import java.util.*;
+import java.io.*;
+public class Sieve
 {
-	int a,b;
-	AB(int x,int y)
-	{
-		a=x;
-		b=y;
-	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int tests=sc.nextInt();
+		int a=tests;
+		int k=1;
+		while(tests-->0)
+		{
+			int n=sc.nextInt();
+            int arr[]=new int[n];
+            Arrays.fill(arr,0);
+            for(int i=2;i*i<=n;i++)
+            {
+               
+              for(int j=2*i;j<n;j=j+i)
+             {  if(arr[j]!=1)
+               	arr[j]=1;
+               k++;}
+            }
+            
+            for(int i=1;i<=100;i++)
+            	if(arr[i]==0)
+            	System.out.println(i);
+System.out.println(k);
+
+
+
+
+
+		}
+
+ }
 }
