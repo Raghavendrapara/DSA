f33a4a1ae87efefd115165fd6a1c358d8a4369e4
raghavendra <raghavendra0360@gmail.com>
2020-10-02 20:06:13 +0530

    Kahn's Algorithm

---
+++ Algorithms/KahnTSort.java
@@ -0,0 +1,61 @@
+/*input
+4
+4
+1 2
+1 3
+2 4
+3 4
+*/
+import java.util.*;
+class KahnTSort{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+		System.out.println("Enter Number Of Vertices");
+		int ver = sc.nextInt();
+
+		for(int ind=0;ind<=ver;ind++)
+			adjList.add(new ArrayList<Integer>());
+
+		System.out.println("Enter Number of Edges");
+		int edges=sc.nextInt();
+		for(int index=1;index<=edges;index++)
+		{
+			int first = sc.nextInt();
+			int second = sc.nextInt();
+			adjList.get(first).add(second);
+		}
+
+		PriorityQueue<Integer> pqueue = new PriorityQueue<>((n1,n2)->(-n1+n2));
+
+		int indegree[] = new int[ver+1];
+		for(ArrayList<Integer> arr:adjList)
+		{
+			for(int val:arr)
+				indegree[val]++;
+		}
+        
+        for(int val=1;val<=ver;val++)
+        	if(indegree[val]==0)
+        		pqueue.add(val);
+
+
+        String sorder="";
+        while(pqueue.size()>0){
+
+        	int curr=pqueue.poll();
+        	sorder = sorder + curr;
+        	for(int i:adjList.get(curr))
+        		{
+        			indegree[i]--;
+        			if(indegree[i]==0){
+        				pqueue.offer(i);
+        			}
+        		}
+        }
+    System.out.println(sorder);
+}
+
+}
